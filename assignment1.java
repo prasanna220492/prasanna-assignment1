fc0edd1a741bc71ef7ac1d662fca41fb0d600b5b
prasanna220492 <prasanna220492@gmail.com>
2016-08-04 11:20:28 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,13 @@
+public class assignment1
+{
+public static void main(String[] args)
+{
+int a = 10;
+double b =4.3;
+System.out.println("the value of a :"+a);
+System.out.println("the value of b :"+b);
+int result =int(a+b);
+System.out.println("the result is :"+result);
+
+}
+}
